5178b4f81acc41ae4f4fd0013c49c919f7de8551
yenv672 <yenv672@newschool.edu>
2015-09-09 16:49:49 -0400

    update to index

--- 01_word_count/Victoria/js/JS.js
+++ 01_word_count/Victoria/js/JS.js
@@ -1,11 +1,26 @@
+
 var nowLine ;
 
 
+function submit(){
+	//console.log("Click");
+	var CountWhat = document.getElementById('textHere').value;
+	//console.log(CountWhat);
+	var resulrHere_Count = document.getElementById('resulrHere_Count');
+	var resulrHere_Summery = document.getElementById('resulrHere_Summery');
+	resulrHere_Count.innerHTML = wordCount(CountWhat);
+	var summeryHere = summery();
+	for(var item in summeryHere){
+		resulrHere_Summery.innerHTML += item +": "+ summeryHere[item]+"<br>";
+	}
+	//resulrHere_Summery.innerHTML = for( var item in summery());
+}
+
 function wordCount(Something){
 	nowLine = Something;
-	var Words_Counter = 0;
+	var Words_Counter = 1;
 	for(var token in Something){
-		if(Something[token] != " "){
+		if(Something[token] == " "){
 			Words_Counter ++;
 		};
 	};
@@ -14,21 +29,25 @@ function wordCount(Something){
 
 function summery(){
 	//console.log(nowLine);
-	var nowWords = "";
-	var dic = {};
-	for(var token in nowLine){
-		
-		if(nowLine[token]== " "){
-			if(!(nowWords in dic)){
+	function nowWordsInDic(nowWords){
+		if(!(nowWords in dic)){
 				dic[nowWords] = 1;
 			}else{
 				dic[nowWords]++;
-				
 			};
+	};
+	var nowWords = "";
+	var dic = {};
+	for(var token in nowLine){
+		if(nowLine[token]== " "){
+			nowWordsInDic(nowWords);
 			nowWords = "";
 		}else{
 			nowWords += [nowLine[token]];
 		};
 	};
+
+	nowWordsInDic(nowWords);
+			nowWords = "";
 	return dic;
 };
