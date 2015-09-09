169c10274e651ca7c65cb0b8b9a0adecb16afcfd
Eric Jiang <erik.jjy@gmail.com>
2015-09-09 01:52:57 -0400

    inital commit, word couter

---
+++ 01_word_count/Eric/script.js
@@ -0,0 +1,44 @@
+function wordCount(rawInput){
+
+	//formating, get rid of puncs and accident inputs
+	var pureText = rawInput.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	pureText = pureText.replace(/\s{2,}/g," ");
+
+	//split words into an array
+	var splitText = pureText.split(" ").sort();
+	
+	console.log ("split text: " + splitText);
+	console.log ("word count: "+ splitText.length + " words");
+
+	var result = frequencyCount (splitText);
+	// console.log(result);
+	for(var i = 0; i < result.length; i++){
+		var obj = result[i];
+		console.log("word: " + obj.text + " | "+"frequency: " + obj.freq);
+	}
+}
+
+
+function frequencyCount (splitText){
+
+	var counter = 1;
+	var resultArray = []; 
+	var resultWord;
+
+	for(var i = 0; i < splitText.length; i++){
+		var currWord = splitText[i];
+		var nextWord = splitText[i+1];
+		if(currWord == nextWord){
+			counter++;
+		}
+		else{
+			resultWord = {
+				text: currWord,
+				freq: counter
+			};
+			resultArray.push(resultWord);
+			counter = 1;
+		}
+	}
+	return resultArray;
+}
