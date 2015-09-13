fc17eb37102d625aaa9c4f45b9d64db7dbc6de68
haotian91 <lihaotian91@gmail.com>
2015-09-13 13:17:31 -0400

    HW1

--- 01_word_count/Haotian/js/main.js
+++ 01_word_count/Haotian/js/main.js
@@ -10,20 +10,36 @@ countWords(content);
 
 var str = content.split(" ");
 
-var counter = 0;
+var words = {};
+var wordsArr = [];
 
-var countNumbers = function(a){
-	for (var i = 0; i<str.length; i++){
-		if(a == str[i]){
-			counter++;
-		};
-	};
+for(var i=0; i<str.length; i++){
+	if(!words.hasOwnProperty(str[i])){
+		words[str[i]]= 1
+	}
+	else{
+		words[str[i]]++
+	}
 };
 
-for (var i=0; i < str.length; i++){
-	countNumbers(str[i]);
-	console.log(str[i] + " : " + counter);
-	counter = 0;
-};
+for (var key in words) {
+		if  (words.hasOwnProperty(key)) {
+			wordsArr.push({word: key, freq: words[key]});
+		}
+	}
+
+wordsArr.sort(function(a, b) {
+		if (a.freq > b.freq) {
+			return -1;
+		}
+		else if (a.freq < b.freq) {
+			return 1;
+		}
+		return 0;
+	});
+
+for (var i=0; i<wordsArr.length ; i++){
+	console.log(wordsArr[i].word + " : " + wordsArr[i].freq);
+}
 
 
