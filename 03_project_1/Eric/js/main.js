7e32c01a2fb2aebc32f628f3e5044e737d6d3a69
Eric Jiang <erik.jjy@gmail.com>
2015-10-07 03:04:01 -0400

    google amp api to uber api

--- 03_project_1/Eric/js/main.js
+++ 03_project_1/Eric/js/main.js
@@ -9,7 +9,6 @@ app.main = (function(){
 		if (geocoder) {
       		geocoder.geocode({ 'address': address[0] }, function (results, status) {
 	         	if (status == google.maps.GeocoderStatus.OK) {
-	         		// latlong.push(results[0].geometry.location);
 	         		latlong.push([results[0].geometry.location.lat(), results[0].geometry.location.lng()]);  
 	         		address.shift();
 	         		if (typeof callback === 'function') {
@@ -33,6 +32,7 @@ app.main = (function(){
 	var fetchLocations = function(obj, addressToUber){
 		if (addressToUber.length == 2) {
 			getUberPrice(addressToUber);
+			addressToUber.length = 0;
 		}
 		else{
 			addressToUber.push(obj);
@@ -40,18 +40,33 @@ app.main = (function(){
 	}
 
 	var getUberPrice = function(addressToUber){
+		console.log(addressToUber[0][0][0], addressToUber[0][0][1]);
 		var startLat = addressToUber[0][0][0];
 		var startLng = addressToUber[0][0][1];
-		var endLat = addressToUber[1][1][0];
-		var endLng = addressToUber[1][1][1];
+		var endLat = addressToUber[1][0][0];
+		var endLng = addressToUber[1][0][1];
+		var serverToken = 'RsQSnU210NSFmoLa1WXsC5QmQe3lQdIsAB5A-b-X';
+		var apiCallLink = "https://api.uber.com/v1/estimates/price?"+"start_latitude="+startLat+"&"+"start_longitude="+startLng+"&"+"end_latitude="+endLat+"&"+"end_longitude="+endLng+"&server_token="+serverToken;
+		$.getJSON(apiCallLink, function(obj){
+			for (var i = 0; i < obj.prices.length; i++) { 
+    			var carType = obj.prices[i].localized_display_name;
+    			if (carType != "Yellow WAV") {
+    				var cost = obj.prices[i].estimate;
+    				var distance = obj.prices[i].distance;
+    				var duration = obj.prices[i].duration;
+    				$('#car-result').append("<p>" + "car: " + carType + " | " +"duration: " + Math.round(duration / 60) + "mins" + " | " + "distance: " + distance + "miles" + " | " + "cost: " + cost + "</p>");
+    			};
+			}
+		});
 	}
 
 	var init = function(){
 		var address = ['' ,''];
 		var addressToUber = [];
 		$('#go-button').off('click').on('click', function(){
-			address[0] = ($('#from').val());
+			address[0] = ($('#from').val()); //address input eg: union square, NY
 			address[1] = ($('#to').val());
+			$('#car-result').html(' ');
 			locate(address, function(obj){
 				fetchLocations(obj, addressToUber);
 			});
